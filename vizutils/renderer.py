f14c74e442352f1b85ff2b3fa8b6aec18c948b15
Gustavo Tandeciarz <gustavo.tandeciarz@salesforce.com>
2022-02-17 16:13:12 -0500

    adding colors

--- vizutils/renderer.py
+++ vizutils/renderer.py
@@ -6,7 +6,8 @@ import graphviz
 class renderer():
 
     def __init__(self, filename:str='') -> None:
-        self.g = graphviz.Digraph('LogViz', comment=f'Log file visualization for {filename}')
+        self.g = graphviz.Digraph('LogViz', comment=f'Log file visualization for {filename}',format='svg')
+        self.useloops = True
 
     def processStack(self, sortedOperations:list):
         g = self.g
@@ -15,40 +16,54 @@ class renderer():
         if(len(self.operations) == 0):
             raise Exception("No operations found in the log file")
         self._checkIdx()
-        self.stackProcessMap = {}
+        # self.stackProcessMap = {}
         g.node('start', label='start', shape='Mdiamond')
         g.node('end', label='end', shape='Msquare')
         for op in self.operations:   
             idx = op.idx
-            uid = op.nodeId
-            if(uid in self.stackProcessMap):
-                self.stackProcessMap[uid].append(op)
-            else:
-                self.stackProcessMap[uid] = [op]
+            # if(uid in self.stackProcessMap):
+            #     self.stackProcessMap[uid].append(op)
+            # else:
+            #     self.stackProcessMap[uid] = [op]
             if(op.eventType == 'APEX'):
                 #add to apex cluster
+                color='0.6 0.700 0.700'
                 pass
             if(op.eventType == 'TRIGGER'):
                 #add to trigger cluster
+                color='purple'
                 pass
             if(op.eventType == 'FLOW'):
                 #add to flow cluster
+                color='red'
                 pass
             if(op.eventType == 'PROCESS BUILDER'):
                 #add to flow cluster
+                color='orange'
                 pass
             
             opType = op.eventType
             opName = op.name.split(':')[0]
             opLabel = opName
+            nodeId = op.nodeId
+            parentNodeId = op.parent.nodeId if op.parent is not None else 'start'
+            
+            if(self.useloops == False):
+                nodeId = f'{op.idx}{nodeId}'
+                parentNodeId = f'{op.idx}{parentNodeId}'
+
+            #create and style the nodes
+            g.node(nodeId, label=opLabel, shape='box', color=color)
+
+            #create and style the edges (arrows to each node)
             if(idx == 0):
-                g.edge('start', op.nodeId, label=f's_{op.idx}')
+                g.edge('start', nodeId, label=f's_{op.idx}', color=color)
                 continue
             
             elif(idx == len(self.operations) - 1):
-                self.g.edge(op.nodeId, 'end', label=f's_{op.idx}')
+                g.edge(nodeId, 'end', label=f's_{op.idx}', color=color)
             else:
-                g.edge(op.parent.nodeId, op.nodeId, label=f's_{op.idx}')
+                g.edge(parentNodeId, nodeId, label=f's_{op.idx}', color=color)
             
             continue
             if(op.get('parent', None) is not None):
